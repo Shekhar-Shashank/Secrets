d754f7fa0c09a924d38fea1c2c79524c1780e604
unknown <shekhars027@gmail.com>
2021-10-02 13:00:05 +0530

    Fix google auth2 redirect Uri's error

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://secret-in.herokuapp.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
       console.log(profile)
